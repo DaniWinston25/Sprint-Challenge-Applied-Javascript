224dbdaa46d8de6e13c50abf3afc4a50c35d720b
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2019-12-15 17:00:37 -0500

    updates to code

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -9,32 +9,22 @@
 //    <div class="tab">topic here</div>
 const topics = document.querySelector(".topics")
 
+topics.appendChild(topicCreator())
 axios 
 .get('https://lambda-times-backend.herokuapp.com/topics')
 .then((res)=>{
-  const topicsArray = res.data
+  user = res.data.topics
 })
 
+// user.forEach((topics)=>{
+//   return topics.
+// })
 
-
- topics.appendChild(topicCreator())
 function topicCreator(){
   const tab = document.createElement('div');
-//   const setTopic = topicsArray.array()
-
-// setTopic[0].text = topicsArray['javascript'];
-// setTopic[1].text = topicsArray['bootstrap'];
-// setTopic[2].text = topicsArray['technology'];
-// setTopic[3].text = topicsArray['jquery'];
-// setTopic[4].text = topicsArray['node.js'];
 
 tab.classList.add('tab');
 
-// tabsClass.appendChild(setTopic[0]);
-// tabsClass.appendChild(setTopic[1]);
-// tabsClass.appendChild(setTopic[2]);
-// tabsClass.appendChild(setTopic[3]);
-// tabsClass.appendChild(setTopic[4]);
 
 return tab;
 }
