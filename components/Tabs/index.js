572aa42f6e2dec2b6c7758c0ac837e4f40388493
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2019-12-15 15:49:45 -0500

    updates

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -15,17 +15,28 @@ axios
   const topicsArray = res.data
 })
 
-function topicCreator(array){
-  const tabClass = document.createElement('div');
-  const setTopic = topicsArray.array()
-
-setTopic[0].text = topicsArray['javascript'];
-setTopic[1].text = topicsArray['bootstrap'];
-setTopic[2].text = topicsArray['technology'];
-setTopic[3].text = topicsArray['jquery'];
-setTopic[4].text = topicsArray['node.js'];
 
 
+ topics.appendChild(topicCreator())
+function topicCreator(){
+  const tab = document.createElement('div');
+//   const setTopic = topicsArray.array()
+
+// setTopic[0].text = topicsArray['javascript'];
+// setTopic[1].text = topicsArray['bootstrap'];
+// setTopic[2].text = topicsArray['technology'];
+// setTopic[3].text = topicsArray['jquery'];
+// setTopic[4].text = topicsArray['node.js'];
+
+tab.classList.add('tab');
+
+// tabsClass.appendChild(setTopic[0]);
+// tabsClass.appendChild(setTopic[1]);
+// tabsClass.appendChild(setTopic[2]);
+// tabsClass.appendChild(setTopic[3]);
+// tabsClass.appendChild(setTopic[4]);
+
+return tab;
 }
 
 
